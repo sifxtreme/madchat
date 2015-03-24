8666c8e1b2c9bfcf30ee521160180c0182fe3f3f
Asif Ahmed <asif@parksleepfly.com>
2015-03-23 20:18:32 -0700

    audio fix

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var concat = require('gulp-concat');
 
 gulp.task('js', function(){
   return gulp.src(["./public/js/jquery.min.js", "./public/js/detectRTC.js", "./public/js/desktop.js", "./public/js/Autolinker.min.js", "./public/js/socket.io.js", "./public/js/variables.js", "./public/js/chat.js", "./public/js/video.js"])
-    .pipe(concat('all-desktop-2.js'))
+    .pipe(concat('all-desktop-3.js'))
     .pipe(gulp.dest('./public/js/'))
     .pipe(uglify())
     .pipe(gulp.dest('./public/js/'))
@@ -15,7 +15,7 @@ gulp.task('js', function(){
 
 gulp.task('js-mobile', function(){
   return gulp.src(["./public/js/jquery.min.js", "./public/js/detectRTC.js", "./public/js/mobile.js", "./public/js/index.js", "./public/js/Autolinker.min.js", "./public/js/socket.io.js", "./public/js/variables.js", "./public/js/chat.js", "./public/js/video.js"])
-    .pipe(concat('all-mobile-2.js'))
+    .pipe(concat('all-mobile-3.js'))
     .pipe(gulp.dest('./public/js/'))
     .pipe(uglify())
     .pipe(gulp.dest('./public/js/'))
--- public/js/video.js
+++ public/js/video.js
@@ -97,7 +97,7 @@ $(document).ready(function(){
     checkVideos();
   }
 
-  comm.connect(id, {audio: false})
+  comm.connect(id, {audio: true})
   comm.on('local', function(options){
     addVideo(options);
   })
